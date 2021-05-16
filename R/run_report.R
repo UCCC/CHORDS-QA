ce782e395197982f8f4f87a2ec388485a0953b51
CartWill <William.A.Carter@cuanschutz.edu>
2021-05-16 01:22:20 -0600

    - Added to pandoc check that will try to download pandoc and store for user.  If fails, will ask user to install manually.  - Updated renv lock file  - Added dependency that may fail to install because it requires compilation

--- R/run_report.R
+++ R/run_report.R
@@ -29,10 +29,6 @@
 #' @export
 
 run_report <- function(priority, dbserver = NULL, dbname = NULL, dbuser = NULL, dbpassword = NULL, outputdir = NULL, batchmode = FALSE, dbport = NULL, dbencrypt = NULL, ...) {
-  if(!rmarkdown::pandoc_version() >= "1.12.3"){
-    stop("Pandoc version 1.12.3  or higher is required.\r\nPlease visit https://www.pandoc.org/installing to install this program before running a CHORDS QA request")
-  }
-
   time_id <- format(Sys.time(), "%Y%m%d_%H%M")
   if(length(dbuser) == 0 || is.null(dbuser)){
     dbuser <- ""
@@ -52,6 +48,40 @@ run_report <- function(priority, dbserver = NULL, dbname = NULL, dbuser = NULL, 
   }
   else{
     assign("outputdir", value = outputdir, envir = .GlobalEnv)
+  }
+
+  # If for some reason the working directory is the windows dir, will change
+  #  it to the output directory.
+  if(tolower(getwd()) %in% tolower(c("C:/Windows/system32"))){
+    setwd(outputdir)
+  }
+
+  # Since pandoc is required to run, checks to see if it is installed.  Will check
+  #  to see if it's in their APPDATA directory.  If not, will try to download it
+  #  to the users APPDATA roaming directory. If still missing, will direct the user to
+  #  download and install it themselves.  Should only be an issue for
+  #  command-line or PMN execution as RStudio comes with pandoc.  Alternatively,
+  #  pandoc can be placed in the working directory of this R program.
+  if(rmarkdown::pandoc_version() < "2.0"){
+    tryCatch({
+        destDir <- file.path(Sys.getenv("APPDATA"), "pandoc")
+        rmarkdown::find_pandoc(dir=destDir)
+        if(rmarkdown::pandoc_version() < "2.0") {
+            githubURL <- "https://github.com/jgm/pandoc/releases/download/2.13/pandoc-2.13-windows-x86_64.zip"
+            destDir <- file.path(Sys.getenv("APPDATA"), "pandoc")
+            dir.create(destDir, showWarnings = FALSE)
+            destFile <- file.path(destDir, "pandoc.zip", sep='')
+            download.file(url=githubURL, destfile=destFile, method="auto")
+            unzip(destFile, overwrite=T, exdir=destDir, junkpaths = T)
+            file.remove(destFile)
+            rmarkdown::find_pandoc(dir=paste(destDir))
+          if(rmarkdown::pandoc_version() < "2.0"){
+            stop("Pandoc version 2.0 or higher is required.\r\nPlease visit https://www.pandoc.org/installing to install this program before running a CHORDS QA request")
+          }
+       }
+    },error = function(cond){
+      stop(paste(cond, "Pandoc version 2.0 or higher is required.\r\nPlease visit https://www.pandoc.org/installing to install this program before running a CHORDS QA request"))
+    })
   }
   if (priority == "P1"){
     if (!is.null(batchmode) && batchmode == TRUE){
